f02ee1f35e3533a8da84100bfc6832449179cd07
tuumej <46086015+tuumej@users.noreply.github.com>
2023-05-15 11:16:02 +0900

    Update Install_Docker_for_Ubuntu.sh
    typo

--- init/Install_Docker_for_Ubuntu.sh
+++ init/Install_Docker_for_Ubuntu.sh
@@ -11,9 +11,7 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 sudo apt-get update
 
