73eb132935a953ec83861213a6743d137a081fea
tuumej <46086015+tuumej@users.noreply.github.com>
2023-06-12 14:51:37 +0900

    Update init_script.sh

--- for_Ubuntu/sh/init_script.sh
+++ for_Ubuntu/sh/init_script.sh
@@ -14,6 +14,8 @@
 # wget https://www.ncloud.com/api/support/download/files/cli/CLI_1.1.14_20230525.zip
 
 # 3. nas ( mkdir / mount )
+## nas acl에 서버 등록
+# ~/sh/nasAcl.sh
 # mkdir /mnt/nas
 # mount -t nfs -o vers=3 nas주소:/nas이름 /마운트경로
 # 예시> sudo mount -t nfs -o vers=3 169.254.84.52:/n2586308_imsinas01 /mnt/nas
